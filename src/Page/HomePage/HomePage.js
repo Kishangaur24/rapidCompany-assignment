27ac6e6826382934e988a7c3d0aac5377520aa74
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-06-30 09:35:24 +0530

    MiddleSection Update

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../../Component/Navbar/Navbar'
+import MiddleSection from '../../Component/MiddleSection/MiddleSection'
 
 const HomePage = () => {
   return (
     <div>
       <Navbar/>
+      <MiddleSection/>
     </div>
   )
 }
